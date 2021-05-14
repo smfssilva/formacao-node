3f45c9fc74124c1b9a040ee62030a6cedb11ed6d
smfssilva <smfssilva@yahoo.com>
2021-05-13 22:04:25 -0300

    insert com knex

--- banco/index.js
+++ banco/index.js
@@ -1,3 +1,26 @@
 const path = require('path')
 const database = require(path.join(__dirname + '/database.js'))
-console.log(__dirname);
+
+/*
+const dados = [
+    {
+        nome: "Call of Durty 2",
+        preco: 89.99    
+    },{
+        nome: "GTA",
+        preco: 90    
+    },{
+        nome: "Wow",
+        preco: 120.99    
+    }
+    
+]
+
+query = database.insert(dados).into("games")
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+*/
